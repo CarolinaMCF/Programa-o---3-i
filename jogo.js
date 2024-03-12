cdc57867a8f0f7517c184edea1eb111c791fb7c6
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2024-03-12 07:53:48 -0300

    ..

--- jogo.js
+++ jogo.js
@@ -1,1 +1,12 @@
 let numeroSecreto = 7;
+
+let chute = prompt('Escolha um número entre 0 e 10.');
+
+
+if(numeroSecreto == chute){
+    alert('Parabéns! Você acertou!');
+    console.log('Parabéns!');
+} else{
+    alert('Você errou...que tristreza!');
+    console.log('Número incorreto.');
+}
