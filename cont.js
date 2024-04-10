5d5bd1bc294493fe7f745740c03ae56553a354f5
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2024-04-10 10:20:40 -0300

    .

--- cont.js
+++ cont.js
@@ -1,4 +1,7 @@
 const botoes = document.querySelectorAll('.objetivos');
+const textos = document.querySelectorAll('.aba-conteudo');
+
+
 console.log(botoes.length);
 
 for (let x = 0; x < botoes.length; x++) {
@@ -6,6 +9,7 @@ for (let x = 0; x < botoes.length; x++) {
     botoes[x].onclick = function() {
       for (let j = 0; j < botoes.length; j++){
       botoes[j].classList.remove('ativo');
+      textos[j].classList.remove('ativa');
     }
     botoes[x].classList.add('ativo');
   }
