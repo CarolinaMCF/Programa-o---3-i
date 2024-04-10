730636492c2e3a5a1de58956e38ca9ad5240d8f5
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2024-04-10 09:25:47 -0300

    mudou x por j

--- cont.js
+++ cont.js
@@ -4,6 +4,9 @@ console.log(botoes.length);
 for (let x = 0; x < botoes.length; x++) {
     console.log(x);
     botoes[x].onclick = function() {
+      for (let j = 0; j < botoes.length; j++)
+      botoes[j].classList.remove('ativo');
+    }
     botoes[x].classList.add('ativo');
   }
 }
