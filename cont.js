a0b64dee93276777681997326ad748e155309b98
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2024-04-24 09:24:53 -0300

    .

--- cont.js
+++ cont.js
@@ -1,7 +1,6 @@
 const botoes = document.querySelectorAll('.objetivos');
 const textos = document.querySelectorAll('.aba-conteudo');
 
-
 console.log(botoes.length);
 
 for (let x = 0; x < botoes.length; x++) {
@@ -21,8 +20,15 @@ const tempoObjetivo1 = new Date ('2025-01-01T00:00:00');
 const tempoObjetivo2 = new Date ('2024-12-10T00:00:00');
 const tempoObjetivo3 = new Date ('2024-11-10T00:00:00');
 const tempoObjetivo4 = new Date ('2024-12-13T00:00:00');
+
+let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+for(let i = 0; i < contadores.length; i++){
+  contadores[i].textContent = CalculaTempo(tempos[i]);
+}
+
+function CalculaTempo(tempoObjetivo) {
 let tempoAtual = new Date ();
-let tempoFinal = tempoObjetivo1 - tempoAtual;
+let tempoFinal = tempoObjetivo - tempoAtual;
 
 let segundos = Math.floor(tempoFinal/1000);
 let minutos = Math.floor(segundos/60);
@@ -32,4 +38,5 @@ let dias = Math.floor(horas/24)
 segundos %= 60;
 minutos %= 60;
 horas %= 24;
-contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos +" segundos ";
+return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos +" segundos ";
+}
